883e9288c8d76832ab18ebd82c3be88dad81b09b
namookk <papakang22@naver.com>
2021-09-11 22:13:39 +0900

    0911 문제풀이 (카카오)

---
+++ Algorithm/src/com/algorithm/day12/Solution4.java
@@ -0,0 +1,24 @@
+package com.algorithm.day12;
+
+public class Solution4 {
+    public static void main(String[] args) {
+        int n = 5;
+        int[] info =  {2,1,1,1,0,0,0,0,0,0,0};
+        //[0,2,2,0,1,0,0,0,0,0,0]
+        solution(n, info);
+    }
+
+    public static int[] solution(int n, int[] info) {
+        int[] answer = {};
+        int apeachScore = 0;
+        int rionScore = 0;
+        for(int i = 0; i < 11; i++){
+
+        }
+
+
+
+
+        return answer;
+    }
+}
