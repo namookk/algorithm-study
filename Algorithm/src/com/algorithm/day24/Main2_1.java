4b49d41ea0d7df30ee5babab8052ba79e9b71522
papakang22 <papakang22@naver.com>
2021-09-23 17:43:40 +0900

    0923 문제풀이

---
+++ Algorithm/src/com/algorithm/day24/Main2_1.java
@@ -0,0 +1,26 @@
+package com.algorithm.day24;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main2_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Integer[] arr = new Integer[n];
+        for(int i = 0 ; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        solution(n, arr);
+    }
+
+    public static void solution(int n, Integer[] arr) {
+        List<Integer> list = new ArrayList<>();
+        int num = Integer.MIN_VALUE;
+        for(Integer i : arr) {
+            if(i > num) System.out.print(i+" ");
+            num = i;
+        }
+    }
+}
