bc808576d250ef4c3d571d30c75bdf4b7f5fb886
papakang22 <papakang22@naver.com>
2021-09-24 10:57:06 +0900

    0924 문제풀이

---
+++ Algorithm/src/com/algorithm/day25/Main2_5.java
@@ -0,0 +1,39 @@
+package com.algorithm.day25;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main2_5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for(int i : solution(n, arr)){
+            System.out.print(i + " ");
+        }
+    }
+    public static List<Integer> solution(int n, int[] arr) {
+        List<Integer> answer = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            String numStr = arr[i] + "";
+            String reverseStr = new StringBuilder(numStr).reverse().toString();
+            int num = Integer.parseInt(reverseStr);
+            if(isPrime(num)) answer.add(num);
+        }
+        return answer;
+    }
+    public static boolean isPrime(int num){
+        if(num == 1){
+            return false;
+        }
+        for(int i = 2; i <= Math.sqrt(num); i++) {
+            if(num % i == 0) return false;
+        }
+        return true;
+    }
+}
+
