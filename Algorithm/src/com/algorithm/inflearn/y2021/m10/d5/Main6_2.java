4614f86722a27261447b7ac4cbf00a68ec24a762
황남욱 <41463678+namookk@users.noreply.github.com>
2021-10-05 22:37:33 +0900

    1005 문제풀이

---
+++ Algorithm/src/com/algorithm/inflearn/y2021/m10/d5/Main6_2.java
@@ -0,0 +1,34 @@
+package com.algorithm.inflearn.y2021.m10.d5;
+
+import java.util.Scanner;
+
+public class Main6_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i : solution(N, arr)) {
+            System.out.print(i +" ");
+        }
+    }
+
+    public static int[] solution(int N, int[] arr) {
+
+        for(int i = 0 ; i < N; i++) {
+            for(int j = 0; j < N-i-1; j++) {
+                if(arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+        return arr;
+    }
+
+}
---
+++ Algorithm/src/com/algorithm/inflearn/y2021/m10/d5/Main6_3.java
@@ -0,0 +1,33 @@
+package com.algorithm.inflearn.y2021.m10.d5;
+
+
+import java.util.Scanner;
+
+public class Main6_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i : solution(N, arr)) {
+            System.out.print(i +" ");
+        }
+    }
+
+    public static int[] solution(int N, int[] arr) {
+        for(int i = 1; i < N; i++) {
+            for(int j = i - 1; j >= 0; j--) {
+                if(arr[j+1] < arr[j]) {
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+}
