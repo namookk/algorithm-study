a75de1112fe65617790a05dcf4da42c04f6afd8c
namookk <papakang22@naver.com>
2021-10-16 22:59:31 +0900

    1016 알고리즘 공부

---
+++ Algorithm/src/com/algorithm/programmers/y2021/m10/d16/Solution.java
@@ -0,0 +1,48 @@
+package com.algorithm.programmers.y2021.m10.d16;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] priorities = {1, 1, 9, 1, 1, 1};
+        int location = 0;
+
+        System.out.println(solution(priorities, location));
+    }
+    public static int solution(int[] priorities, int location) {
+        int answer = 0;
+        Queue<Printer> queue = new LinkedList<>();
+        List<Integer> sortedList = Arrays.stream(priorities)
+                .boxed()
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+
+        for(int i = 0 ; i < priorities.length; i++) {
+            queue.offer(new Printer(priorities[i] , i == location));
+        }
+
+        while(!queue.isEmpty()){
+            Printer printer = queue.poll();
+            if(sortedList.get(0) > printer.priority) {
+                queue.offer(printer);
+            }else{
+                answer++;
+                sortedList.remove(0);
+                if(printer.mine) break;
+            }
+        }
+
+        return answer;
+    }
+}
+
+class Printer{
+    int priority;
+    boolean mine;
+
+    public Printer(int _priority, boolean _mine) {
+        priority = _priority;
+        mine = _mine;
+    }
+}
