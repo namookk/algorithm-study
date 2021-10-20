f5416d64148557306915259bc58f9cc39ad5de57
papakang22 <papakang22@naver.com>
2021-10-20 18:20:05 +0900

    1020 문제풀이

---
+++ Algorithm/src/com/algorithm/programmers/y2021/m10/d20/Solution.java
@@ -0,0 +1,20 @@
+package com.algorithm.programmers.y2021.m10.d20;
+
+public class Solution {
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println(solution(n));
+    }
+
+    public static int solution(int n) {
+        int answer = 0;
+        return getMin(n-1);
+    }
+
+    public static int getMin(int k) {
+        for(int i = 2; i <= k;i++) {
+            if(k%i == 0) return i;
+        }
+        return 0;
+    }
+}
