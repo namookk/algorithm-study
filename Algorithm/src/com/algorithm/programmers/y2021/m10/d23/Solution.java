1138d74c09294ff87d3fd4f291d102f7095eea60
namookk <papakang22@naver.com>
2021-10-23 22:28:25 +0900

    1023 알고리즘 공부

---
+++ Algorithm/src/com/algorithm/programmers/y2021/m10/d23/Solution.java
@@ -0,0 +1,29 @@
+package com.algorithm.programmers.y2021.m10.d23;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        int brown = 24;
+        int yellow = 24;
+
+        System.out.println(Arrays.toString(solution(brown, yellow)));
+    }
+    public static int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        int sum = brown + yellow;
+        for(int i = 3; i < (sum/2 + 1); i++) {
+            if(sum % i == 0) {
+                int width = Math.max(i, sum/i);
+                int height = Math.min(i, sum/i);
+                if(brown == (width * 2 - 1) + (height * 2 - 3)) {
+                    answer[0] = width;
+                    answer[1] = height;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
