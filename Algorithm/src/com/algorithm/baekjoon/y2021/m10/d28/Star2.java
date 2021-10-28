5f58c76d61d2ece2f87a9a5b3e3363ca03a0c3ce
papakang22 <papakang22@naver.com>
2021-10-28 11:56:36 +0900

    1028 알고리즘 별찍기

---
+++ Algorithm/src/com/algorithm/baekjoon/y2021/m10/d28/Star2.java
@@ -0,0 +1,26 @@
+package com.algorithm.baekjoon.y2021.m10.d28;
+
+import java.util.Scanner;
+
+public class Star2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        solution(n);
+    }
+    public static void solution(int n ){
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= (n * 2); j++) {
+                if(i % 2 == 0) {
+                    if( j % 2 == 0) System.out.print("*");
+                    else System.out.print(" ");
+                }else{
+                    if( j % 2 == 1) System.out.print("*");
+                    else System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
