e5f10a4709d31f36982d470be1f170ffe25fb4b4
namookk <papakang22@naver.com>
2021-11-01 22:43:15 +0900

    1101 알고리즘 공부 weekly10 완성

--- Algorithm/src/com/algorithm/programmers/y2021/m11/d1/Solution.java
+++ Algorithm/src/com/algorithm/programmers/y2021/m11/d1/Solution.java
@@ -11,48 +11,73 @@ public class Solution {
         }
     }
     public static String[] solution(int[][] lines) {
-        String[] answer = {};
         List<Point> pointList = new ArrayList<>();
-        for(int i = 0; i < lines.length; i++) {
-            double a1 = lines[i][0];
-            double b1 = lines[i][1];
-            double c1 = lines[i][2];
-            for(int j = i + 1; j < lines.length; j++) {
-                double a2 = lines[j][0];
-                double b2 = lines[j][1];
-                double c2 = lines[j][2];
+        List<String> pointStrList = new ArrayList<>();
+        long minY = Long.MAX_VALUE;
+        long minX = Long.MAX_VALUE;
+        long maxY = Long.MIN_VALUE;
+        long maxX = Long.MIN_VALUE;
 
-                double x = (b1*c2 - b2*c1)/(a1*b2 - a2*b1);
-                double y = -a1/b1*(b1*c2-b2*c1)/(a1*b2-a2*b1)-c1/b1;
-                if((x == (int)x) && (y == (int)y)){
-                    pointList.add(new Point((int)x, (int)y));
+        for(int i = 0; i < lines.length - 1; i++) {
+            long A = lines[i][0];
+            long B = lines[i][1];
+            long E = lines[i][2];
+            for(int j = i + 1; j < lines.length; j++) {
+                long C = lines[j][0];
+                long D = lines[j][1];
+                long F = lines[j][2];
+
+                long division = (A*D - B*C);
+                if(division == 0) continue;
+
+                double x = (double)(B*F - E*D)/division;
+                double y = (double)(E*C - A*F)/division;
+
+                if((x - (long)x == 0) && (y - (long)y == 0)){
+                    Point p = new Point((long)x, (long)y);
+
+                    minY = Math.min(minY, p.y);
+                    minX = Math.min(minX, p.x);
+                    maxY = Math.max(maxY, p.y);
+                    maxX = Math.max(maxX, p.x);
+
+                    pointList.add(p);
+                    pointStrList.add(p.x+","+p.y);
                 }
             }
         }
-        int minY = Integer.MAX_VALUE;
-        int minX = Integer.MAX_VALUE;
-        int maxY = Integer.MIN_VALUE;
-        int maxX = Integer.MIN_VALUE;
-        for(Point p : pointList){
-            minY = Math.min(minY, p.y);
-            minX = Math.min(minX, p.x);
-            maxY = Math.max(maxY, p.y);
-            maxX = Math.max(maxX, p.x);
+
+        String[] answer = new String[(int)(maxY - minY + 1)];
+        int i = 0;
+        while(maxY >= minY) {
+            long currX = minX;
+            StringBuffer sb = new StringBuffer();
+            while(currX <= maxX) {
+                if(pointStrList.indexOf(currX+","+maxY) > -1) sb.append("*");
+                else sb.append(".");
+                currX++;
+            }
+            answer[i] = sb.toString();
+            i++;
+            maxY--;
         }
-        int[][] cdn = new int[(maxY - minY)+1][(maxX - minX)+1];
-        for(Point p : pointList){
-        }
-        System.out.println(minY + " , " + minX + " , " + maxY + " , " + maxX);
 
         return answer;
     }
 }
 
 class Point{
-    int x,y;
+    long x,y;
 
-    public Point(int _x, int _y){
+    public Point(long _x, long _y){
         x = _x;
         y = _y;
+    }
+
+    public boolean isEqual(long _x, long _y){
+        if(x == _x && y == _y) {
+            return true;
+        }
+        return false;
     }
 }
