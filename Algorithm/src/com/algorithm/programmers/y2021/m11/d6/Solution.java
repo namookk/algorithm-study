108822c3542a3f441df997801f586b604d1620b0
namookk <papakang22@naver.com>
2021-11-06 22:37:47 +0900

    1106 알고리즘 공부 BFS 완성

--- Algorithm/src/com/algorithm/programmers/y2021/m11/d6/Solution.java
+++ Algorithm/src/com/algorithm/programmers/y2021/m11/d6/Solution.java
@@ -1,19 +1,21 @@
 package com.algorithm.programmers.y2021.m11.d6;
 
+import java.awt.*;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Solution {
     public static void main(String[] args) {
-        int[][] maps = {{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{0,0,0,0,1}};
+        int[][] maps = {{1, 0, 1, 1, 1}, {1, 0, 1, 0, 1}, {1, 0, 1, 1, 1}, {1, 1, 1, 0, 0}, {0, 0, 0, 0, 1}};
 
         System.out.println(solution(maps));
     }
 
     public static int solution(int[][] maps) {
-        int answer = 0;
-        int[][] ch = new int[5][5];
+        int n = maps.length;
+        int m = maps[0].length;
+        int[][] ch = new int[n][m];
         Queue<Point> queue = new LinkedList<>();
         queue.offer(new Point(0, 0));
         int level = 1;
@@ -23,12 +25,12 @@ public class Solution {
             int len = queue.size();
             for(int i = 0; i < len; i++) {
                 Point point = queue.poll();
-                if(point.x == 4 && point.y == 4) return level;
-                if(point.y < 4 && maps[point.y + 1][point.x] == 1 && ch[point.y +1][point.x] == 0) {
+                if(point.x == m-1 && point.y == n-1) return level;
+                if(point.y < n-1 && maps[point.y + 1][point.x] == 1 && ch[point.y +1][point.x] == 0) {
                     queue.offer(new Point(point.x, point.y + 1));
                     ch[point.y +1][point.x] = 1;
                 }
-                if(point.x < 4 && maps[point.y][point.x + 1] == 1  && ch[point.y][point.x + 1] == 0) {
+                if(point.x < m-1 && maps[point.y][point.x + 1] == 1  && ch[point.y][point.x + 1] == 0) {
                     queue.offer(new Point(point.x + 1, point.y));
                     ch[point.y][point.x + 1] = 1;
                 }
