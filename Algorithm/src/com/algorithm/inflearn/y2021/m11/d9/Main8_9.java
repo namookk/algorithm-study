cee72fd6693abf1023159451aea464d7274c5b09
papakang22 <papakang22@naver.com>
2021-11-17 11:19:06 +0900

    1117 DFS 미로 경로개수 찾기

--- Algorithm/src/com/algorithm/inflearn/y2021/m11/d9/Main8_9.java
+++ Algorithm/src/com/algorithm/inflearn/y2021/m11/d9/Main8_9.java
@@ -1,59 +1,53 @@
 package com.algorithm.inflearn.y2021.m11.d9;
 
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class Main8_9 {
+    static int[][] arr = new int[8][8];
+    static int answer = 0;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[][] arr = new int[8][8];
         for(int i = 1; i < 8; i++) {
             for(int j = 1; j < 8; j++) {
                arr[i][j] = sc.nextInt();
             }
         }
-
-        System.out.println(solution(arr));
+        System.out.println(solution());
     }
 
-    public static int solution(int[][] arr) {
-        int answer = 0;
+    public static int solution() {
         int[][] ch = new int[8][8];
-        Queue<Point> queue = new LinkedList<>();
-        queue.offer(new Point(1,1));
         ch[1][1] = 1;
-
-        while(!queue.isEmpty()) {
-            int len = queue.size();
-            for(int i = 0; i < len; i++ ) {
-                Point p = queue.poll();
-                if(p.x == 7 && p.y == 7) {
-                    answer++;
-                    continue;
-                }
-                if(p.x > 1 && arr[p.y][p.x - 1] == 0 && ch[p.y][p.x - 1] == 0) {
-                    queue.offer(new Point(p.x - 1, p.y));
-                    ch[p.y][p.x - 1] = 1;
-                }
-                if(p.x < 7 && arr[p.y][p.x + 1] == 0 && ch[p.y][p.x + 1] == 0) {
-                    queue.offer(new Point(p.x + 1, p.y));
-                    ch[p.y][p.x + 1] = 1;
-                }
-                if(p.y > 1 && arr[p.y - 1][p.x] == 0 && ch[p.y - 1][p.x] == 0) {
-                    queue.offer(new Point(p.x, p.y - 1));
-                    ch[p.y - 1][p.x] = 1;
-                }
-                if(p.y < 7 && arr[p.y + 1][p.x] == 0 && ch[p.y + 1][p.x] == 0) {
-                    queue.offer(new Point(p.x, p.y + 1));
-                    ch[p.y + 1][p.x] = 1;
-                }
-            }
-        }
-
+        DFS(new Point(1,1), ch);
         return answer;
     }
 
+    public static void DFS(Point p, int[][] ch) {
+        if(p.x == 7 && p.y == 7) {
+            answer++;
+        }else {
+            if (p.x > 1 && arr[p.y][p.x - 1] == 0 && ch[p.y][p.x - 1] == 0) { //LEFT
+                ch[p.y][p.x - 1] = 1;
+                DFS(new Point(p.x - 1, p.y), ch);
+                ch[p.y][p.x - 1] = 0;
+            }
+            if (p.x < 7 && arr[p.y][p.x + 1] == 0 && ch[p.y][p.x + 1] == 0) { //RIGHT
+                ch[p.y][p.x + 1] = 1;
+                DFS(new Point(p.x + 1, p.y),ch);
+                ch[p.y][p.x + 1] = 0;
+            }
+            if (p.y > 1 && arr[p.y - 1][p.x] == 0 && ch[p.y - 1][p.x] == 0) { //UP
+                ch[p.y - 1][p.x] = 1;
+                DFS(new Point(p.x, p.y - 1), ch);
+                ch[p.y - 1][p.x] = 0;
+            }
+            if (p.y < 7 && arr[p.y + 1][p.x] == 0 && ch[p.y + 1][p.x] == 0) { //DOWN
+                ch[p.y + 1][p.x] = 1;
+                DFS(new Point(p.x, p.y + 1), ch);
+                ch[p.y + 1][p.x] = 0;
+            }
+        }
+    }
 }
 
 class Point{
