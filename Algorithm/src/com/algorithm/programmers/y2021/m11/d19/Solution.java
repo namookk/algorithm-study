5da6a7d37905c2cfc8889ec8958beb12905fc72f
namookk <papakang22@naver.com>
2021-11-19 11:26:41 +0900

    알고리즘 공부

---
+++ Algorithm/src/com/algorithm/programmers/y2021/m11/d19/Solution.java
@@ -0,0 +1,24 @@
+package com.algorithm.programmers.y2021.m11.d19;
+
+public class Solution {
+    public static void main(String[] args) {
+        String s = "bababa";
+        System.out.println(solution(s));
+    }
+
+    public static int solution(String s)
+    {
+        String[] arr = {"aa","bb","cc","dd","ee","ff","gg","hh","ii","jj","kk","ll","nn","mm","oo","pp","qq","rr","ss","tt","uu","vv","ww","xx","yy","zz"};
+        while(true){
+            int cnt = 0;
+            for(String str : arr) {
+                while(s.indexOf(str) > -1) {
+                    cnt++;
+                    s = s.replace(str, "");
+                }
+            }
+            if(cnt == 0) break;
+        }
+        return s.length() > 0 ? 0 : 1;
+    }
+}
