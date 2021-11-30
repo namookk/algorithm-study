29d7cb8a4014f87308aff4582e62c60af2a0f1a3
namookk <papakang22@naver.com>
2021-11-30 21:12:44 +0900

    refactoring : Inflearn Chapter10-1 recursive -> dynamic Programming 으로 변경

--- Algorithm/src/com/algorithm/inflearn/chapter10/Main10_1.java
+++ Algorithm/src/com/algorithm/inflearn/chapter10/Main10_1.java
@@ -3,13 +3,19 @@ package com.algorithm.inflearn.chapter10;
 import java.util.Scanner;
 
 public class Main10_1 {
+
     static int answer = 0;
     static int n;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
+        //재귀방법
         recursive(0);
         System.out.println(answer);
+
+        // 동적 계획법
+        System.out.println(dynamic(n));
     }
     //재귀방법
     public static void recursive(int sum) {
@@ -20,5 +26,15 @@ public class Main10_1 {
                 recursive(sum + i);
             }
         }
+    }
+    //동적계획법
+    public static int dynamic(int n) {
+        int[] dy = new int[n+1];
+        dy[1] = 1;
+        dy[2] = 2;
+        for(int i = 3; i <= n; i++) {
+            dy[i] = dy[i-2] + dy[i-1];
+        }
+        return dy[n];
     }
 }
