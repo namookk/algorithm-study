01ff66fb8c6b05b781ec76cdf1b9134df734b736
namookk <papakang22@naver.com>
2021-12-14 18:00:22 +0900

    solve : Inflearn Algorithm chapter6 1번 문제 풀이

---
+++ Algorithm/src/com/algorithm/inflearn2/chapter6/Main6_1.java
@@ -0,0 +1,39 @@
+package com.algorithm.inflearn2.chapter6;
+
+import java.util.Stack;
+
+public class Main6_1 {
+    public static void main(String[] args) {
+        String[] cmds = {"5","-2","4","C","D","9","+","+"};
+
+        System.out.println(solve(cmds));
+    }
+
+    public static int solve(String[] cmds) {
+        Stack<Integer> stack = new Stack<>();
+        for(String cmd : cmds) {
+            if(cmd.equals("+")) {
+               int num1 = stack.pop();
+               int num2 = stack.pop();
+               stack.add(num2);
+               stack.add(num1);
+               stack.add(num1 + num2);
+            }else if(cmd.equals("C")){
+                stack.pop();
+            }else if(cmd.equals("D")){
+                int num = stack.pop();
+                stack.add(num);
+                stack.add(num * 2);
+            }else{
+                stack.add(Integer.parseInt(cmd));
+            }
+        }
+        int answer = 0;
+        while(!stack.isEmpty()){
+            int num = stack.pop();
+            answer += num;
+            System.out.print(num + " ");
+        }
+        return answer;
+    }
+}
