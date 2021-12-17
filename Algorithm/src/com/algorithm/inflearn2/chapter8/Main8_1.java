bfff950dbab5933e898a2597d588717799317751
namookk <papakang22@naver.com>
2021-12-17 12:00:09 +0900

    re_solve : Inflearn Chapter8 동적계획법 1번 문제 다시 풀기

--- Algorithm/src/com/algorithm/inflearn2/chapter8/Main8_1.java
+++ Algorithm/src/com/algorithm/inflearn2/chapter8/Main8_1.java
@@ -9,27 +9,17 @@ public class Main8_1 {
         System.out.println(solve(m, n));
     }
     public static int solve(int m, int n)  {
-        int[][] dy = new int[m][n];
-        int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                dy[i][j] = Integer.MAX_VALUE;
-            }
-        }
-        dy[0][0] = 0;
+        int[][] dy = new int[m+1][n+1];
+        int[][] dirs = {{0, -1}, {-1, 0}};
+        dy[1][1] = 1;
 
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(i == m && j == m) continue;
-                for(int[] dir : dirs) {
-                    int x = i + dir[0];
-                    int y = j + dir[1];
-                    if(x < 0 || y < 0 || x == m || y == n || dy[x][y] == Integer.MAX_VALUE) continue;
-                    dy[i][j] = Math.min(dy[i][j], dy[x][y] + 1);
-                }
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(i == 1 && j == 1) continue;
+                dy[i][j] = dy[i-1][j] + dy[i][j-1];
             }
         }
 
-        return dy[m-1][n-1];
+        return dy[m][n];
     }
 }
