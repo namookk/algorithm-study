606a8487eb950ec637c0314021858b7aae3cb3b2
namookk <papakang22@naver.com>
2021-12-18 13:30:22 +0900

    re_solve : Inflearn Chapter8 동적계획법 4번문제 풀이 강의보고 다시 풀기

--- Algorithm/src/com/algorithm/inflearn2/chapter8/Main8_4.java
+++ Algorithm/src/com/algorithm/inflearn2/chapter8/Main8_4.java
@@ -4,8 +4,8 @@ public class Main8_4 {
     public static void main(String[] args) {
         int[] nums = {1,2,3,4,5,2,6,10,4,12};
         int[] nums2 = {0,1,0,3,2,3};
-        System.out.println(solve(nums));
-        System.out.println(solve(nums2));
+        System.out.println(re_solve(nums));
+        System.out.println(re_solve(nums2));
     }
 
     public static int solve(int[] nums) {
@@ -22,8 +22,25 @@ public class Main8_4 {
                 dy[i] = dy[i-1];
                 maxarr[i] = maxarr[i-1];
             }
-            System.out.println(dy[i] +" , " + maxarr[i]);
         }
         return dy[nums.length];
+    }
+
+    public static int re_solve(int[] nums) {
+        int n = nums.length;
+        int[] dy = new int[n];
+        dy[0] = 1;
+        int max = 1;
+        for(int i = 1; i < n; i++) {
+            dy[i] = 1;
+            for(int j = 0; j < i; j++) {
+                if(nums[j] < nums[i]) {
+                    dy[i] = Math.max(dy[i], dy[j] + 1);
+                }
+            }
+            max = Math.max(max, dy[i]);
+        }
+
+        return max;
     }
 }
