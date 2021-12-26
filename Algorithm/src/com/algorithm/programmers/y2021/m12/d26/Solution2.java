4a042238dc0122c557ed72262e7ca8257116edb3
namookk <papakang22@naver.com>
2021-12-27 00:18:07 +0900

    solve : Programmers Level3 베스트앨범 해쉬 문제풀이

--- Algorithm/src/com/algorithm/programmers/y2021/m12/d26/Solution2.java
+++ Algorithm/src/com/algorithm/programmers/y2021/m12/d26/Solution2.java
@@ -2,6 +2,23 @@ package com.algorithm.programmers.y2021.m12.d26;
 
 import java.util.*;
 
+//https://programmers.co.kr/learn/courses/30/lessons/42579
+//정확성  테스트
+//    테스트 1 〉	통과 (3.24ms, 72.1MB)
+//    테스트 2 〉	통과 (4.15ms, 79.8MB)
+//    테스트 3 〉	통과 (2.87ms, 84.8MB)
+//    테스트 4 〉	통과 (2.69ms, 72.6MB)
+//    테스트 5 〉	통과 (3.06ms, 73.8MB)
+//    테스트 6 〉	통과 (2.99ms, 74.6MB)
+//    테스트 7 〉	통과 (3.61ms, 77.7MB)
+//    테스트 8 〉	통과 (3.64ms, 74.6MB)
+//    테스트 9 〉	통과 (3.10ms, 76.4MB)
+//    테스트 10 〉	통과 (2.99ms, 77.8MB)
+//    테스트 11 〉	통과 (2.94ms, 73.8MB)
+//    테스트 12 〉	통과 (3.08ms, 80.5MB)
+//    테스트 13 〉	통과 (3.24ms, 73.3MB)
+//    테스트 14 〉	통과 (3.40ms, 73.6MB)
+//    테스트 15 〉	통과 (2.98ms, 72.5MB)
 public class Solution2 {
     public static void main(String[] args) {
         String[] genres = {"classic", "pop", "classic", "classic", "pop"};
