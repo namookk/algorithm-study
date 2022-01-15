a8e0667681a1dfe04d95d9bbdc33b6069aa03401
namookk <papakang22@naver.com>
2022-01-15 19:30:35 +0900

    solve : 백준 Silver5 1037번 약수 문제 풀이

---
+++ Algorithm/src/com/algorithm/baekjoon/silver5/Solution_1037.java
@@ -0,0 +1,17 @@
+package com.algorithm.baekjoon.silver5;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution_1037 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[0] * arr[arr.length - 1]);
+    }
+}
