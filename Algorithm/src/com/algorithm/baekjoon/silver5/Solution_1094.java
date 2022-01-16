c329f85ca2b5414299a32885acd2504ec4ff2a7e
namookk <papakang22@naver.com>
2022-01-16 10:10:54 +0900

    solve : 백준 Silver5 1094번 막대기 문제 풀이

---
+++ Algorithm/src/com/algorithm/baekjoon/silver5/Solution_1094.java
@@ -0,0 +1,11 @@
+package com.algorithm.baekjoon.silver5;
+
+import java.util.Scanner;
+
+public class Solution_1094 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(Integer.bitCount(n));
+    }
+}
