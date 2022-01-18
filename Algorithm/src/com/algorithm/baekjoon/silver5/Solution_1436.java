f2610a757a6438bba91984eef4f473cc7251e7d8
namookk <papakang22@naver.com>
2022-01-18 11:01:11 +0900

    solve : 백준 Silver5 1436번 영화감독 숌 문제 풀이

---
+++ Algorithm/src/com/algorithm/baekjoon/silver5/Solution_1436.java
@@ -0,0 +1,16 @@
+package com.algorithm.baekjoon.silver5;
+
+import java.util.Scanner;
+
+public class Solution_1436 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int num = 666;
+        int cnt = 1;
+        while(cnt < n){
+            if((++num + "").contains("666")) cnt++;
+        }
+        System.out.println(num);
+    }
+}
