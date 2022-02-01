be59d909882e53248e3b68456741f97544f7c798
namookk <papakang22@naver.com>
2022-02-01 18:30:46 +0900

    solve : 피보나치 수열 다시 풀기 다이나믹 프로그래밍 사용

--- Algorithm/src/com/algorithm/inflearn/chapter2/Main2_4.java
+++ Algorithm/src/com/algorithm/inflearn/chapter2/Main2_4.java
@@ -10,12 +10,23 @@ public class Main2_4 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         System.out.println(solution(n));
+        for(int i : re_solution(n)) System.out.print(i + " ");
     }
     public static String solution(int n) {
         String answer = "";
         for(int i = 1; i <= n; i++) {
             System.out.print(pibonachi(i) + " ");
         }
+        return answer;
+    }
+    public static int[] re_solution(int n){
+        int[] answer = new int[n];
+        answer[0] = 1;
+        answer[1] = 1;
+        for (int i = 2; i < n; i++) {
+            answer[i] = answer[i-2] + answer[i-1];
+        }
+
         return answer;
     }
 
